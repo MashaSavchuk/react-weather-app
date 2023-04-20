772aa86f36885546ae5ee1dde228a6256f86b200
Maria Savchuk <savchuk_maria@ukr.net>
2023-04-21 00:36:48 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        <p>
+          This project is coded by{" "}
+          <a
+            href="https://cute-starburst-57982c.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mariia Savchuk
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/MashaSavchuk/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-source on GitHub
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
