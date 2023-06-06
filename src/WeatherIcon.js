37b3ca8e3afe9484f70ad0e21387f17146ad93be
Maria Savchuk <savchuk_maria@ukr.net>
2023-06-07 00:18:29 +0100

    Added dynamic weather icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,16 +2,33 @@ import React from "react";
 import ReactAnimatedWeather from "react-animated-weather-updated";
 
 export default function WeatherIcon(props) {
+  // console.log(props.code);
+  const codeMapping = {
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "few-clouds-day": "PARTLY_CLOUDY_DAY",
+    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-day": "PARTLY_CLOUDY_DAY",
+    "scattered-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "broken-clouds-day": "CLOUDY",
+    "broken-clouds-night": "CLOUDY",
+    "shower-rain-day": "RAIN",
+    "shower-rain-night": "RAIN",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "thunderstorm-day": "WIND",
+    "thunderstorm-night": "WIND",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "FOG",
+    "mist-night": "FOG",
+  };
   return (
     <ReactAnimatedWeather
-      icon="CLEAR_DAY"
+      icon={codeMapping[props.code]}
       color="goldenrod"
-      size={64}
+      size={70}
       animate={true}
     />
-    // <img
-    //   src="https://raw.githubusercontent.com/divyanshu013/react-animated-weather/HEAD/react-animated-weather.gif"
-    //   alt="props.alt"
-    // ></img>
   );
 }
